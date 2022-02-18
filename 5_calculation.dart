e65d0ac6e102e8de8a8700df7e3260c47d6839e6
shhain <shahin@ishraak.com>
2022-02-18 18:04:05 +0600

    first assignment

---
+++ 5_calculation.dart
@@ -0,0 +1,22 @@
+// 05. Write a Dart program to print the sum (addition), multiply, subtract,
+//divide and remainder of two numbers.
+
+void main() {
+  int num1 = 8;
+  int num2 = 3;
+  
+  int sum = num1 + num2;
+  print('Sum: $sum');
+  
+  int mul = num1 * num2;
+  print('Multiply: $mul');
+  
+  int sub = num1 - num2;
+  print('Subtract: $sub');
+  
+  double div = num1 / num2;
+  print('Divide: $div');
+  
+  int rem = num1 % num2;
+  print('Remainder: $rem');
+}
