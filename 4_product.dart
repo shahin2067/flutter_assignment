cc59f7947a333dc418bf6389499740649343f4e7
shahin <shahin@ishraak.com>
2022-02-25 17:48:19 +0600

    assignment 2 one file added

--- 4_product.dart
+++
@@ -1,7 +0,0 @@
-// 04. Write a Dart program that takes two numbers and displays the product of two numbers.
-
-void main() {
-  int a = 5, b = 10;
-  int mul = a * b;
-  print('Product is: $mul');
-}
