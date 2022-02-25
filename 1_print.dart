e8c880474e2a5f4cbda830f8b0e1f7900821762a
shahin <shahin@ishraak.com>
2022-02-25 17:14:00 +0600

    assignment one

--- 1_print.dart
+++
@@ -1,9 +0,0 @@
-
-
-// 01 Write a Dart program to print 'Hello' on screen and then print your name on a separate line.
-
-void main() {
-  
-  print('Hello');
-  print('Shahin');
-}
--- 2_two_numbers_sum.dart
+++
@@ -1,9 +0,0 @@
-// 02. Write a Dart program to print the sum of two numbers.
-
-void main() {
-  
-  int num1 = 10, num2 = 5;
-  int sum = num1 + num2;
-  
-  print('Sum: $sum');
-}
